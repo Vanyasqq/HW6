5e61a57ad318b9935616a6653854789e596e8285
ivan <ivan.kh.cook@gmail.com>
2022-10-25 12:19:39 +0500

    asd

---
+++ HW6.1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+string[] num = Console.ReadLine().Split(new char[] { ',' });
+int count =0;
+for(int i=0;i<num.Length;i++)
+{
+int inum = System.Convert.ToInt32(num[i]);
+    
+    if(inum > 0)
+    {
+        count ++;
+    }
+}
+Console.WriteLine(count);
+
---
+++ HW6.2/Program.cs
@@ -0,0 +1,21 @@
+﻿double СoordinateInput(string massage)
+{
+    Console.WriteLine(massage);
+    var Сoordinate = Convert.ToDouble(Console.ReadLine());
+    return Сoordinate;
+}
+
+
+double k1 = СoordinateInput("Введите координату k1");
+double k2 = СoordinateInput("Введите координату k2");
+double b1 = СoordinateInput("Введите координату b1");
+double b2 = СoordinateInput("Введите координату b2");
+ 
+ 
+var x = -(b1 - b2) / (k1 - k2);
+var y = k1 * x + b1;
+ 
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+ 
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
